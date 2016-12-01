621af6065e72b7f93eb228841fe4aa1388f86161
Mike G <mkgalligar@gmail.com>
2016-12-01 15:41:03 -0800

    12/1/16 lots of changes to look&feel

--- app/scripts/services/pics.js
+++ app/scripts/services/pics.js
@@ -11,7 +11,7 @@ angular.module('watts4000project02eweatherappApp')
 .factory('pics', function ($resource) {
   // Service logic
   // ...
-
+////////////////////https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=35edbca3494716bac72f7f2719ccb167&tags=:Sunny&tag_mode=any&text=skyline&sort=relevance&lat=41.8781&lon=-87.6298&radius=2&extras=url_l&format=json&nojsoncallback=1
   // Public API here
   return $resource('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=35edbca3494716bac72f7f2719ccb167&tags=:tags&tag_mode=any&text=skyline&sort=relevance&lat=:lat&lon=:lon&radius=2&extras=url_l&format=json&nojsoncallback=1', {}, {
     query: {
