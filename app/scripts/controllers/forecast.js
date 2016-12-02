cfd68731b75bcd70bc34a1a43831288e3bd07981
Mike G <mkgalligar@gmail.com>
2016-12-01 22:12:15 -0800

    12/1/16 5th commit

--- app/scripts/controllers/forecast.js
+++ app/scripts/controllers/forecast.js
@@ -8,53 +8,47 @@
  * Controller of the watts4000project02eweatherappApp
  */
 angular.module('watts4000project02eweatherappApp')
-.controller('ForecastCtrl', function ($scope, $routeParams, forecast, pics) {
-  $scope.cityID = $routeParams.cityID;
+  .controller('ForecastCtrl', function($scope, $routeParams, forecast, pics) {
+    $scope.cityID = $routeParams.cityID;
 
 
     $scope.forecastData = forecast.query({
-        cityID: $scope.cityID
+      cityID: $scope.cityID
     });
 
-///Start Weather icons//////////
-$scope.getWeatherImage = function(weatherSummary){
-    var weatherImage = 'default';
+    ///Start Weather icons//////////
+    $scope.getWeatherImage = function(weatherSummary) {
+      var weatherImage = 'default';
 
-    if(weatherSummary === 'Rain'){
+      if (weatherSummary === 'Rain') {
         weatherImage = 'rain';
 
-    } else if (weatherSummary === 'Sunny'){
+      } else if (weatherSummary === 'Sunny') {
         weatherImage = 'sun';
 
-      } else if (weatherSummary === 'Clear'){
-          weatherImage = 'clear';
+      } else if (weatherSummary === 'Clear') {
+        weatherImage = 'clear';
 
-        } else if (weatherSummary === 'Clouds'){
-            weatherImage = 'clouds';
+      } else if (weatherSummary === 'Clouds') {
+        weatherImage = 'clouds';
 
-      } else if (weatherSummary === 'Snow'){
-          weatherImage = 'snow';
-
-
-    } //  … etc adding else if statements until you get them all…
-    return weatherImage;
-};
-///End Weather icons//////////$("input[name*='nation']")   <img ng-src= "images/{{getWeatherImage(prediction.weather[0].main)}}" style="width:80px;">
+      } else if (weatherSummary === 'Snow') {
+        weatherImage = 'snow';
 
 
-  ////////////////////////////////////////////
-  $scope.forecastData.$promise.then(function(data){
+      } //  … etc adding else if statements until you get them all…
+      return weatherImage;
+    };
+    ///End Weather icons//////////
+
+
+    $scope.forecastData.$promise.then(function(data) {
       $scope.pics = pics.query({
-          lat: data.city.coord.lat, // Note: this reference to data.lat is probably wrong and should refer to the lat value in the forecast data object.
-          lon: data.city.coord.lon, // Note: see note above re: path to this data in the forecastData results
-          tags: data.list[3].weather.description // Note: This will also need to be pathed correctly in the forecastData
+        lat: data.city.coord.lat, // Note: this reference to data.lat is probably wrong and should refer to the lat value in the forecast data object.
+        lon: data.city.coord.lon, // Note: see note above re: path to this data in the forecastData results
+        tags: data.list[3].weather.description // Note: This will also need to be pathed correctly in the forecastData
       });
 
-
-
+    });
 
   });
-  /////////////////////////////////////////////
-
-
-});
--- app/scripts/services/forecast.js
+++ app/scripts/services/forecast.js
@@ -8,20 +8,20 @@
  * Factory in the watts4000project02eweatherappApp.
  */
 angular.module('watts4000project02eweatherappApp')
-.factory('forecast', function ($resource) {
-  // Service logic
-  // ... 
-  // 16 day forecast:http://api.openweathermap.org/data/2.5/forecast/daily?id=:cityID&cnt=16&units=imperial&APPID=600ee3b60b8fe48d87def46be2f0e45f
-  //forecast API call withOUT variables:.....
-  //                http://api.openweathermap.org/data/2.5/forecast/daily?id=4717560&cnt=5&units=imperial&APPID=600ee3b60b8fe48d87def46be2f0e45f
-  // Public API here
-  return $resource('http://api.openweathermap.org/data/2.5/forecast/daily?id=:cityID&cnt=5&units=imperial&APPID=600ee3b60b8fe48d87def46be2f0e45f', {}, {
-    query: {
-      method:'GET',
-      params:{
-        cityID: '4717560' // Paris, France ID
-      },
-      isArray:false
-    }
+  .factory('forecast', function($resource) {
+    // Service logic
+    // ... 
+    // 16 day forecast:http://api.openweathermap.org/data/2.5/forecast/daily?id=:cityID&cnt=16&units=imperial&APPID=600ee3b60b8fe48d87def46be2f0e45f
+    //forecast API call withOUT variables:.....
+    //                http://api.openweathermap.org/data/2.5/forecast/daily?id=4717560&cnt=5&units=imperial&APPID=600ee3b60b8fe48d87def46be2f0e45f
+    // Public API here
+    return $resource('http://api.openweathermap.org/data/2.5/forecast/daily?id=:cityID&cnt=5&units=imperial&APPID=600ee3b60b8fe48d87def46be2f0e45f', {}, {
+      query: {
+        method: 'GET',
+        params: {
+          cityID: '4717560' // Paris, France ID
+        },
+        isArray: false
+      }
+    });
   });
-});
